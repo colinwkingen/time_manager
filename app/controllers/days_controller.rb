ee5a8220166ac8e56ac7898b4d689666ce6f8dae
Colin Kingen <colinwkingen@gmail.com>
2016-09-09 10:52:50 -0700

    give days basic CRUD functionality, create partial for error message and creation form

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,52 @@
+class DaysController < ApplicationController
+
+  def index
+    @days = Day.all
+    render :index
+  end
+
+  def show
+    @day = Day.find(params[:id])
+    render :show
+  end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @day = Day.new(day_params)
+    if @day.save
+      redirect_to  days_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @day = Day.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @day = Day.find(params[:id])
+    if @day.update(day_params)
+      redirect_to days_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @day = Day.find(params[:id])
+    @day.destroy
+    redirect_to days_path
+  end
+
+  private
+  def day_params
+    params.require(:day).permit(:date)
+  end
+
+end
