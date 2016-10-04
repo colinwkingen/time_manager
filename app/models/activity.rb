8b565e5942091309952259bb883917b94d6792e6
Colin Kingen <colinwkingen@gmail.com>
2016-10-04 16:37:17 -0700

    break up time intosecond/minute/hour with model method

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,12 @@ class Activity < ActiveRecord::Base
   belongs_to :day
   validates :name, :presence => true
 
+
+  def return_time
+    minutes = self.total_time / 60
+    hours = minutes / 60
+    seconds = self.total_time - ( hours * 180 )
+    return [seconds, minutes, hours]
+  end
+
 end
