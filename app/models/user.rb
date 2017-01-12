a528d2032b523cb6bf6ca665cbc19b0490f2f380
Colin Kingen <colinwkingen@gmail.com>
2017-01-12 10:05:50 -0800

    begin developing method on day model to return only current user activity data at index

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,23 @@ class User < ApplicationRecord
     @gibbon = Gibbon::Request.new
     subscribe = @gibbon.lists(list_id).members.create(body: { email_address: self.email, status: "subscribed", double_optin: true })
   end
+
+  # Should return totals of all activities and totals of activity time for the user as a hash
+  # instead returns empty hash. FIX. Gets a small number now...
+
+  def activity_totals
+    activity_arr = {}
+    self.days.each do |day|
+      # byebug
+      day.activities.each do |activity|
+        key = activity.name
+        if activity_arr.key?(:key)
+          activity_arr[:key] += activity.total_time
+        else
+          activity_arr[:key] = activity.total_time
+        end
+      end
+    end
+    activity_arr
+  end
 end
